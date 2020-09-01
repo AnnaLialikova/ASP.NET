2436ba6ca2ee494e0b84d3ba035ba5b83abb2ae7
Ляликова Анна <65788392+AnnaLialikova@users.noreply.github.com>
2020-09-01 15:12:40 +0300

    LAB1

---
+++ WebApplication1/GuestResponce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class GuestResponce
+    {
+        public int GuestResponceIdId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+        public DateTime RData { get; set; }
+        public GuestResponce()
+        {
+
+        }
+
+        public GuestResponce(string name, string email, string phone, bool? willattend)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            WillAttend = willattend;
+            RData = DateTime.Now;
+        }
+
+    }
+}
